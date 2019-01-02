c4c416b3805e3e28d21f395b4f3b84f625f775ec
Annabel Church <215145+arc64@users.noreply.github.com>
2019-01-02 04:43:14 +0100

    Check win condition

--- index.js
+++ index.js
@@ -19,19 +19,57 @@ const game = () => {
         return board;
     };
 
+    const matches = (tokens) => {
+        if (tokens[0] == tokens[1] && tokens[0] == tokens[2]){
+            return true;
+        }
+        return false;
+    };
+
+    const hasWinCondition = (board) => {
+        // assume that win is check on each turn, 
+        // winner is assumed to be last turn haver
+
+        // TODO: could just check the rows/columns/diagonals 
+        // that last token was placed
+        
+        const row1 = board[0];
+        const row2 = board[1];
+        const row3 = board[2];
+
+        if (matches(row1) || matches(row2) || matches(row3))
+            return true;
+        
+        const colummn1 = [board[0][0], board[1][0], board[2][0]];
+        const colummn2 = [board[0][1], board[1][1], board[2][1]];
+        const colummn3 = [board[0][2], board[1][2], board[2][2]];
+
+        if (matches(colummn1) || matches(colummn2) || matches(colummn3))
+            return true;
+
+        const diagonal1 = [board[0][0], board[1][1], board[2][2]];
+        const diagonal2 = [board[0][2], board[1][1], board[2][0]];
+
+        if (matches(diagonal1) || matches(diagonal2))
+            return true;
+        
+        return false
+    };
+
     // players
     let player1 = 'X';
     let player2 = 'O';
     let board = newBoard;
 
     // ask player for token
-    // takeTurn
-    // check win
+    // takeTurn and then check for win 
     // if not win change player
+
 
     return {
         newBoard: newBoard,
-        takeTurn: takeTurn
+        takeTurn: takeTurn, 
+        hasWinCondition: hasWinCondition
     }
 };
 
--- index.test.js
+++ index.test.js
@@ -2,8 +2,11 @@ const env = require('./index');
 const game = env();
 
 const emptyGame = [ [ null, null, null ],
-                      [ null, null, null ],
-                      [ null, null, null ] ];
+                    [ null, null, null ],
+                    [ null, null, null ] ];
+
+const player1 = 'X';
+const player2 = 'O';
 
 test('when game starts, board is empty', () => {
   expect(env().newBoard()).toEqual(emptyGame);
@@ -13,13 +16,12 @@ test('when game starts, board is empty', () => {
 
 test('when player plays a token in a slot that is not taken, the board is updated', () => {   const game = env(); 
   let board = game.newBoard();
-  let player1 = 'X';
   let row = 1;
   let column = 2;
 
   const turnTaken = [ [ null, null, null ],
-                    [ null, null, player1 ],
-                    [ null, null, null ] ];
+                      [ null, null, player1 ],
+                      [ null, null, null ] ];
 
   expect(board).toEqual(emptyGame);
   expect(game.takeTurn(player1, board, row, column)).toEqual(turnTaken);
@@ -27,30 +29,54 @@ test('when player plays a token in a slot that is not taken, the board is update
 
 test('when player plays a token in a slot that is taken, the board remains unchanged', () => {
   let board = game.newBoard();
-  let player1 = 'X';
-  let player2 = 'O';
   let row = 1;
   let column = 2;
 
-  const firstTurnTaken = [ [ null, null, null ],
-                    [ null, null, player2 ],
-                    [ null, null, null ] ];
+  const firstTurnTaken = [  [ null, null, null ],
+                            [ null, null, player2 ],
+                            [ null, null, null ] ];
 
   const secondTurnTaken = [ [ null, null, null ],
-                    [ null, null, player1 ],
-                    [ null, null, null ] ];
+                            [ null, null, player1 ],
+                            [ null, null, null ] ];
 
   expect(board).toEqual(emptyGame);
   expect(game.takeTurn(player2, board, row, column)).toEqual(firstTurnTaken);
   expect(game.takeTurn(player1, board, row, column)).not.toEqual(secondTurnTaken);
 });
 
-// test('when player plays a token in a slot that is taken, the user is notified', () => {
+test('when player plays a token in a slot that creates a win condition, the game is won', () => {
+  let board = [ [ null, null, player1 ],
+                [ null, player2, player2 ],
+                [ player1, null, null ] ];
+  let row = 1;
+  let column = 0;
+
+  let turnTaken = [ [ null, null, player1 ],
+                [ player2, player2, player2 ],
+                [ player1, null, null ] ];
+
+  expect(game.hasWinCondition(board)).toEqual(false);
+  expect(game.takeTurn(player2, board, row, column)).toEqual(turnTaken);
+  expect(game.hasWinCondition(board)).toEqual(true);
+  
+});
+
+// test('when player plays a token in a slot that creates no win condition, the game switches player', () => {
 //   expect(1).toBe(2);
 // });
 
 
 
-// test('when player plays a token in a slot that is not taken, the game switches player', () => {
+
+// test('when player plays a token in a slot that is taken, the user is notified', () => {
+//   expect(1).toBe(2);
+// });
+
+// test('when player plays a token in a slot that creates a win condition, the player is notified', () => {
+//   expect(1).toBe(2);
+// });
+
+// test('when the game switches player, the player is notified', () => {
 //   expect(1).toBe(2);
 // });
